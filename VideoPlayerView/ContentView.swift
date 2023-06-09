b1a5d479aad7c3c03152e83c2f06cf168af87cbb
LeoBKChen <bangkangchen@gmail.com>
2023-06-09 10:13:16 +0800

    Initial Commit

---
+++ VideoPlayerView/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  VideoPlayerView
+//
+//  Created by 陳邦亢 on 2023/6/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VideoPlayerView/VideoPlayerViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlayerViewApp.swift
+//  VideoPlayerView
+//
+//  Created by 陳邦亢 on 2023/6/9.
+//
+
+import SwiftUI
+
+@main
+struct VideoPlayerViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
